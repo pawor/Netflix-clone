6ea87f1758e7bd130aee170c912e1fd644ad928f
pawor <pawor.emi@gmail.com>
2022-04-16 22:12:31 +0700

    slide movie

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,7 @@
 import { Movie } from '../typings'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import Thumbnail from '../components/Thumbnail'
+import { useRef, useState } from 'react'
 
 interface Props {
     title: string
@@ -8,21 +9,45 @@ interface Props {
 }
 
 function Row({title,movies}:Props) {
+    const rowRef = useRef<HTMLDivElement>(null)
+    const [isMoved, setIsMoved] = useState(false)
+
+    const handleClick = (direction: string) => {
+        setIsMoved(true)
+
+        if (rowRef.current){
+            const { scrollLeft, clientWidth} = rowRef.current
+
+            const scrollTo = direction === 'left' ? scrollLeft-clientWidth : scrollLeft+clientWidth
+            
+            rowRef.current.scrollTo({
+                left: scrollTo,
+                behavior: 'smooth'
+            })
+        }
+    }
+    
+    console.log(rowRef.current!.scrollLeft, rowRef.current!.clientWidth)
+    
     return (
         <div className='h-40 space-y-0.5 md:space-y-2'>
             <h2 className='w-56 text-sm font-semibold cursor-pointer text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>{title}</h2>
             <div className='relative group md:-ml-2'>
                 <ChevronLeftIcon 
-                    className='absolute top-0 bottom-0 z-40 m-auto transition opacity-0 cursor-pointer left-2 h-9 w-9 hover:scale-125 group-hover:opacity-100'/>
+                    className={`absolute top-0 bottom-0 z-40 m-auto transition opacity-0 cursor-pointer left-2 h-9 w-9 hover:scale-125 group-hover:opacity-100 ${!isMoved && 'hidden'}`}
+                    onClick={() => handleClick("left")}
+                />
                     
-                <div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 scrollbar-hide'>
+                <div ref={rowRef} className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 scrollbar-hide'>
                     {movies.map((movie) => (
                         <Thumbnail key={movie.id} movie={movie} />
                     ))}
                 </div>
                 
                 <ChevronRightIcon 
-                    className='absolute top-0 bottom-0 z-40 m-auto transition opacity-0 cursor-pointer left-2 h-9 w-9 hover:scale-125 group-hover:opacity-100'/>
+                    className={`absolute top-0 bottom-0 z-40 m-auto transition opacity-0 cursor-pointer right-2 h-9 w-9 hover:scale-125 group-hover:opacity-100 ${!isMoved}`}
+                    onClick={() => handleClick("right")}
+                />
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ interface Props {
 
 const Home = ({netflixOriginals,actionMovies,comedyMovies,documentaries,horrorMovies,romanceMovies,topRated,trendingNow}:Props) => {
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] '>
+    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -30,7 +30,7 @@ const Home = ({netflixOriginals,actionMovies,comedyMovies,documentaries,horrorMo
       
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals}/>
-        <section>
+        <section className='md:space-y-24'>
           <Row title='Trending Now' movies={trendingNow} />
           <Row title='Top Rated' movies={topRated} />
           <Row title='Action Thrillers' movies={actionMovies} />
